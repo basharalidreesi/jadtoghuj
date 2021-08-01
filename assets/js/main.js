9d8ec79d76a44ac71d9e8e675893e2b48404047d
bashar <basharalidreesi@gmail.com>
2021-08-01 21:47:05 +0300

    u

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,14 @@ const jad = {
         },
 
         initAllScripts: function() {
-                jad.viewport.reportViewportHeight();
+                jad.viewport.initViewportScripts();
         },
 
         viewport: {
 
+                initViewportScripts: function() {
+                        jad.viewport.reportViewportHeight();
+                },
                 reportViewportHeight: function() {
                         let height = window.innerHeight * 0.01;
                         jad.viewport.setViewportHeight(height);
@@ -22,6 +25,41 @@ const jad = {
 
         },
 
+        util: {
+		dTimer: 0,
+		debounce: function(callback, delay) {
+			clearTimeout(jad.util.dTimer);
+			return jad.util.dTimer = setTimeout(callback, delay);
+		},
+		tTimer: 0,
+		throttle: function(callback, delay) {
+			if (jad.util.tTimer) { return; }
+			return jad.util.tTimer = setTimeout(() => {
+				if (callback) {
+					callback();
+				}
+				jad.util.tTimer = 0;
+			}, delay);
+		},
+		clamp: function(min, number, max) {
+			return Math.max(min, Math.min(number, max));
+		},
+		parabola: function(a, x, b, c) {
+			/* https://www.desmos.com/calculator */
+			return a * (x + b) ** 2 + c;
+		},
+		randomIntBetween: function(min, max) {
+			min = Math.ceil(min);
+			max = Math.floor(max);
+			return Math.floor(Math.random() * (max - min) + min);
+		},
+		randomFloatBetween: function(min, max) {
+			return Math.random() * (max - min) + min;
+		},
+		queryMedia: function(query) {
+			return window.matchMedia(query).matches;
+		},
+	},
 
 }
 
