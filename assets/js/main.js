2a9ade577d9161fe93536061f45f258573dee265
bashar <basharalidreesi@gmail.com>
2021-08-21 15:20:09 +0300

    u

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,8 @@ const jad = {
                 observeSentinel: function() {
                         if (!jad.lexicon.sentinel) { return; }
                         let observer = new IntersectionObserver(([e]) => {
-                                jad.lexicon.header.classList.toggle('header--smol', e.intersectionRatio < 1), {threshold: [1]}
+                                jad.lexicon.header.classList.toggle('header--smol', e.intersectionRatio < 1),
+                                { threshold: [1] }
                         });
                         observer.observe(jad.lexicon.sentinel);
                 }
