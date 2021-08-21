892289cebc4e0bbb47bedf7442f7daac80fdbb5e
bashar <basharalidreesi@gmail.com>
2021-08-21 16:10:14 +0300

    u

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,8 +68,9 @@ const jad = {
                         if (jad.lexicon.header.clientHeight == 45) { return; }
                         window.addEventListener("scroll", () => {
                                 if (jad.lexicon.header.getBoundingClientRect().top <= 15) {
-                                        let scaleRatio = jad.util.clamp(0, window.pageYOffset / window.innerHeight, 1);
-                                        console.log(scaleRatio);
+                                        // let scaleRatio = jad.util.clamp(0, window.pageYOffset / window.innerHeight, 1);
+                                        // console.log(scaleRatio);
+                                        console.log(jad.lexicon.header.scrollTop);
                                 }
                         });
                 },
