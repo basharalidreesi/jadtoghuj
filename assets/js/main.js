bf7a23e54a7e51576c77ace5279b308475f33961
bashar <basharalidreesi@gmail.com>
2021-08-21 16:33:12 +0300

    u

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,7 @@ const jad = {
                         if (jad.lexicon.header.clientHeight == 45) { return; }
                         window.addEventListener("scroll", () => {
                                 if (jad.lexicon.header.getBoundingClientRect().top <= 15) {
-                                        console.log(window.scrollTop);
+                                        console.log(window.pageYOffset);
                                         // let scaleRatio = jad.util.clamp(0.5, window.pageYOffset / window.innerHeight, 1);
                                         // console.log(scaleRatio);
                                 }
