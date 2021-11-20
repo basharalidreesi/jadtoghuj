48bc7946b09c0d6f955b022093aef4432ad61a12
bashar <basharalidreesi@gmail.com>
2021-11-20 11:55:26 +0200

    u

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,7 +262,7 @@ const jad = {
                 scoutNewProjects: function() {
                         jad.lexicon.lookCards.forEach((card) => {
                                 let previousMonth = new Date();
-                                previousMonth.setMonth(nextMonth.getMonth() - 1);
+                                previousMonth.setMonth(previousMonth.getMonth() - 1);
                                 let datePublished = card.getAttribute("data-jad-date-published").split("-");
                                 let processedDatePublished = new Date(datePublished[2], datePublished[1] - 1, datePublished[0]);
                                 let processedDateTimestamp = processedDatePublished.getTime();
